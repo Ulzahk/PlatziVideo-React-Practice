b84f965606845f9b889c978b5ccd7f4f125a2791
Ulzahk <ulzahk@gmail.com>
2020-10-22 08:57:49 -0500

    [CREATED] ESLint structure and sass webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,53 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   resolve: {
-      extensions: ['.js', '.jsx', '.ico'],
+    extensions: [".js", ".jsx", ".ico"],
   },
   module: {
-      rules: [{
-              test: /\.(js|jsx)$/,
-              exclude: /node_modules/,
-              use: {
-                  loader: 'babel-loader'
-              }
-          },
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
           {
-              test: /\.html$/,
-              use: [{
-                  loader: 'html-loader'
-              }]
-          }
-      ]
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+				test: /\.(s*)css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					'css-loader',
+					'sass-loader'
+				]
+			},
+    ],
   },
   plugins: [
-      new HtmlWebPackPlugin({
-          template: './public/index.html',
-          filename: './index.html',
-          favicon: './public/favicon.ico'
-      }),
-  ]
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+      favicon: "./public/favicon.ico",
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'assets/[name].css'
+		})
+  ],
 };
