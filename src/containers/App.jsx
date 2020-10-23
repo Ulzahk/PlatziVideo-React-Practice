406344f1045163739ce416993d04f8953efba324
Ulzahk <ulzahk@gmail.com>
2020-10-23 08:54:27 -0500

    [FIXED] some eslint issues in App.jsx

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Header from "../components/Header";
-import Search from "../components/Search";
-import Categories from "../components/Categories";
-import Carousel from "../components/Carousel";
-import CarouselItem from "../components/CarouselItem";
-import Footer from "../components/Footer";
-import "../assets/styles/App.scss";
-import "../assets/styles/Media.scss";
-import useInitialState from "../hooks/useInitialState";
+import React from 'react';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+import '../assets/styles/App.scss';
+import '../assets/styles/Media.scss';
+import useInitialState from '../hooks/useInitialState';
 
 const Endpoint1 = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.KEY}&language=es-MX&page=1`;
 const Endpoint2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.KEY}&language=es-MX&page=1`;
@@ -17,11 +17,11 @@ const App = () => {
   const initialState2 = useInitialState(Endpoint2);
   const initialState3 = useInitialState(Endpoint3);
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
       <Search />
       {initialState.total_pages > 0 && (
-        <Categories title="Mi Lista">
+        <Categories title='Mi Lista'>
           <Carousel>
             {initialState.results.slice(16, 19).map((item) => (
               // eslint-disable-next-line react/jsx-props-no-spreading
@@ -30,7 +30,7 @@ const App = () => {
           </Carousel>
         </Categories>
       )}
-      <Categories title="Populares">
+      <Categories title='Populares'>
         <Carousel>
           {initialState.results.map((item) => (
             // eslint-disable-next-line react/jsx-props-no-spreading
@@ -39,7 +39,7 @@ const App = () => {
         </Carousel>
       </Categories>
 
-      <Categories title="Mejores Calificaciones">
+      <Categories title='Mejores Calificaciones'>
         <Carousel>
           {initialState2.results.map((item) => (
             // eslint-disable-next-line react/jsx-props-no-spreading
@@ -48,7 +48,7 @@ const App = () => {
         </Carousel>
       </Categories>
 
-      <Categories title="Estrenos">
+      <Categories title='Estrenos'>
         <Carousel>
           {initialState3.results.map((item) => (
             // eslint-disable-next-line react/jsx-props-no-spreading
