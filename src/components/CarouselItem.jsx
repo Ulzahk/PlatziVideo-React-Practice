ad8b7aa201465cfa8d931f2f955e75dc10162dbc
Ulzahk <ulzahk@gmail.com>
2020-10-28 18:14:08 -0500

    [UPDATED] add and delete favorite

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,45 +1,72 @@
 /* eslint-disable */
 import React from 'react';
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types';
+import { setFavorite, deleteFavorite } from '../actions'
 import '../assets/styles/components/CarouselItem.scss';
 import playButton from '../assets/static/icons8-play-64.png';
 import plusButton from '../assets/static/icons8-plus-64.png';
+import removeIcon from '../assets/static/remove-icon.png';
 
 
-const CarouselItem = ({ poster_path, original_title, release_date, vote_average, vote_count }) => (
-  <div className='carousel-item'>
-    <img
-      className='carousel-item__img'
-      src={'//image.tmdb.org/t/p/w220_and_h330_face' + poster_path}
-      alt={original_title}
-    />
-    <div className='carousel-item__details'>
-      <div>
-        <img
-          className='carousel-item__details--img'
-          src={playButton}
-          alt='Botón de Play'
-        />
-        <img
-          className='carousel-item__details--img'
-          src={plusButton}
-          alt='Botón de Agregar'
-        />
+const CarouselItem = (props) => {
+  const { id, cover, title, year, contentRating, duration } = props;
+  const handleSetFavorite = () => {
+    props.setFavorite({
+        id, cover, title, year, contentRating, duration
+    })
+  }
+  const handleDeleteFavorite = (itemId) => {
+    props.deleteFavorite(itemId)
+  }
+  return(
+    <div className='carousel-item'>
+      <img
+        className='carousel-item__img'
+        src={cover}
+        alt={title}
+      />
+      <div className='carousel-item__details'>
+        <div>
+          <img
+            className='carousel-item__details--img'
+            src={playButton}
+            alt='Botón de Play'
+          />
+          <img
+            className='carousel-item__details--img'
+            src={plusButton}
+            alt='Botón de Agregar'
+            onClick={handleSetFavorite}
+          />
+          <img
+            className='carousel-item__details--img'
+            src={removeIcon}
+            alt='Botón de Eliminar'
+            onClick={() => handleDeleteFavorite(id)}
+          />
+        </div>
+        <p className='carousel-item__details--title'>{title}</p>
+        <p className='carousel-item__details--subtitle'>
+          {`${year} | ${contentRating} | ${duration}`}
+        </p>
       </div>
-      <p className='carousel-item__details--title'>{original_title}</p>
-      <p className='carousel-item__details--subtitle'>
-        {`${release_date} | ${vote_average} | Votos: ${vote_count}`}
-      </p>
     </div>
-  </div>
-);
-
-CarouselItem.propTypes = {
-  poster_path: PropTypes.string,
-  original_title: PropTypes.string,
-  release_date: PropTypes.string,
-  vote_average: PropTypes.number,
-  vote_count: PropTypes.number,
+  );
 };
 
-export default CarouselItem;
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+};
+
+const mapDispatchToProps = {
+  setFavorite,
+  deleteFavorite,
+}
+
+export default connect(null, mapDispatchToProps)(CarouselItem);
+
